cb9aab37c2af541db0848963e343ee33c122e1fb
Henroed <henroed@protonmail.com>
2022-10-31 14:14:57 +0100

    Starta event.php

---
+++ public/Client/HTML/event.php
@@ -0,0 +1,46 @@
+<html>
+<link rel="stylesheet" href="../Design/css/registrer.css">
+<div class="container">
+      <div class="wrapper">
+        <div class="title"><span>Snikk Snakk og Pølsevev
+            <?php
+                //your PHP code goes here
+
+            ?></span></div>
+        <form action="#">
+          <div class="row">
+            <i class="fas fa-info"></i>
+            Info om Snikk Snakk.
+            <?php
+                //your PHP code goes here
+
+            ?>
+          </div>
+          <div class="row">
+            <i class="fas fa-dato"></i>
+            Dato ting tang.
+            <?php
+                //your PHP code goes here
+
+            ?>
+          </div>
+          <div class="row">
+            <i class="fas fa-bilde"></i>
+            <img scr="..\Design\img\Capture.PNG" width="auto" height="auto">
+            <?php
+                //your PHP code goes here
+
+            ?>
+          </div>
+          <div class="row button">
+            <input type="submit" value="Kommer">
+          </div>
+        </form>
+      </div>
+    </div>
+ <?php
+//your PHP code goes here
+
+?>
+ </body>
+ </html>
