5be91a4b6600a93b23fc7f155f48edcdaeb7b827
Henroed <henroed@protonmail.com>
2022-11-11 16:57:15 +0100

    Fiks Event til Database

--- public/Client/HTML/homepage.php
+++ public/Client/HTML/homepage.php
@@ -1,11 +1,14 @@
 <link rel="stylesheet" href="../Design/css/main.css">
 <?php include 'inc/header.html' ?>
   <?php
+      $conn = mysqli_connect("localhost", "root", "", "eventdatabase");  
       $i = 0;
-      while($i < 5) {
+      
+      while($i <= 100) {
         include '../../PHP/inc/event.php';
       $i = $i + 1;
       }
+      $conn->close();
     ?>
 <?php include 'inc/footer.html' ?>
 </div>
