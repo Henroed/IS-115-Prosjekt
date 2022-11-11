c381b45daa4c070cd2efec43301fdf4b70f9b815
Henroed <henroed@protonmail.com>
2022-11-11 17:33:10 +0100

    Session start i Login

--- public/Client/HTML/myEvents.php
+++ public/Client/HTML/myEvents.php
@@ -1,9 +1,12 @@
 <?php include 'inc/header.html' ?>
 <?php
-      $i = 0;
-      while($i < 5) {
-        include '../../PHP/inc/event.php';
-      $i = $i + 1;
-      }
-    ?>
+       $conn = mysqli_connect("localhost", "root", "", "eventdatabase");  
+       $i = 0;
+       
+       while($i <= 100) {
+         include '../../PHP/inc/event.php';
+       $i = $i + 1;
+       }
+       $conn->close();
+     ?>
 <?php include "inc/footer.html" ?>
--- public/Client/HTML/profile.php
+++ public/Client/HTML/profile.php
@@ -6,70 +6,50 @@
     <!---<title> Responsive Registration Form | CodingLab </title>--->
     <link rel="stylesheet" href="../Design/css/registrer.css">
 <?php include 'inc/header.html' ?>
+<?php 
+    $conn = mysqli_connect("localhost", "root", "", "eventdatabase");  
+
+    $sql = "SELECT fornavn, etternavn, epost, tlf, city, zip, kjønn FROM user WHERE userID = 18";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+?>
   <div class="container">
     <div class="title">Min profil</div>
     <div class="content">
       <form action="register" method="POST">
         <div class="user-details">
           <div class="input-box">
-            <span class="details">Fornavn</span>
-            <input type="text" name="fornavn" placeholder="Chris" required>
+            <span class="details"><?php echo $row["fornavn"] . " " . $row["etternavn"]?></span>
           </div>
           <div class="input-box">
-            <span class="details">Etternavn</span>
-            <input type="text" name="etternavn" placeholder="Martin">
+            <span class="details"><?php echo $row["epost"] ?></span>
           </div>
           <div class="input-box">
-            <span class="details">Epost</span>
-            <input type="text" name="epost" placeholder="@mail.com" required>
+            <span class="details"><?php echo $row["tlf"] ?></span>
           </div>
           <div class="input-box">
-            <span class="details">Tlf nummer</span>
-            <input type="text" name="tlf" placeholder="+47 123 45 678" required>
+            <span class="details"><?php echo $row["city"] ?></span>
           </div>
           <div class="input-box">
-            <span class="details">By</span>
-            <input type="text" name="city" placeholder="Kristiansand" required>
+            <span class="details"><?php echo $row["zip"] ?></span>
           </div>
           <div class="input-box">
-            <span class="details">Zip-kode</span>
-            <input type="text" name="zip" placeholder="46xx" required>
+            <span class="details"><?php echo $row["kjønn"] ?></span>
           </div>
-          <div class="input-box">
-            <span class="details">Passord</span>
-            <input type="text" name="passord" placeholder="Skriv inn passord" required>
-          </div>
-          <div class="input-box">
-            <span class="details">Bekreft passord</span>
-            <input type="text" placeholder="Gjenta ditt passord" >
-          </div>
-        </div>
-        <div class="gender-details">
-          <input type="radio" name="gender" id="dot-1">
-          <input type="radio" name="gender" id="dot-2">
-          <input type="radio" name="gender" id="dot-3">
-          <span class="gender-title">Kjønn</span>
-          <div class="category">
-            <label for="dot-1" value="Mann">
-            <span class="dot one"></span>
-            <span class="gender">Mann</span>
-          </label>
-          <label for="dot-2" value="Dame">
-            <span class="dot two"></span>
-            <span class="gender">Dame</span>
-          </label>
-          <label for="dot-3" value="annet">
-            <span class="dot three"></span>
-            <span class="gender">Annet</span>
-            </label>
-          </div>
-        </div>
         <div class="button">
           <input type="submit" value="Lagre endret informasjon">
         </div>
       </form>
     </div>
 </div>
+<?php   
+    }
+  }
+  $conn->close();
+?>
 <?php include "inc/footer.html" ?>
 </body>
 </html>
