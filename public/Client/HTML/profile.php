cb8f5571da01477a32ead3613d8af7686d32529b
RaRe-14 <79587633+RaRe-14@users.noreply.github.com>
2022-11-11 17:19:36 +0100

    fixed header + footer

--- public/Client/HTML/profile.php
+++ public/Client/HTML/profile.php
@@ -1,8 +1,4 @@
-<!DOCTYPE html>
-<!-- Created By CodingLab - www.codinglabweb.com -->
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="UTF-8">
+
     <!---<title> Responsive Registration Form | CodingLab </title>--->
     <link rel="stylesheet" href="../Design/css/registrer.css">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
