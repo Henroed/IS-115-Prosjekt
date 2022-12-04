bfcfe42901ad340c8ad53fa3e22967eef7c3f065
RaRe-14 <79587633+RaRe-14@users.noreply.github.com>
2022-12-04 15:06:56 +0100

    eventKommer.php funker!

--- public/Client/HTML/eventKommer.php
+++ public/Client/HTML/eventKommer.php
@@ -3,16 +3,12 @@
 require_once('../../../private/Database/inc/db_connect.php');
 $conn = mysqli_connect("localhost", "root", "", "eventdatabase");  
 
-$eventID = 2;
-$sql = "SELECT COUNT(*) FROM myevent where eventID=$eventID";
+$eventID=4; //byttes ut med $_POST["eventID"]
+$sql = "SELECT COUNT(eventID) as resultat FROM myevent WHERE eventID = '$eventID'";
 
-$result = $conn->query($sql);
-print_r($result);
- 
-if($pdo->lastInsertId() > 0) {
-  echo "Data satt i databasen, identifisert med UID " . $pdo->lastInsertId() . ".";
-} else {
-  echo "Data ikke satt i databasen.";
-}
+$result = mysqli_query($conn, $sql);
+$value = mysqli_fetch_assoc($result);
+echo implode($value);
+
 
 ?>
