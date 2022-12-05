05eb30175c987ebc5cf00e75ee0387c27401ad5a
RaRe-14 <79587633+RaRe-14@users.noreply.github.com>
2022-12-05 10:56:10 +0100

    oppdatert db fil

--- private/Database/eventdatabase.sql
+++ private/Database/eventdatabase.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Nov 02, 2022 at 06:54 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Host: 127.0.0.1
+-- Generation Time: Dec 05, 2022 at 10:55 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,12 +29,46 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `event` (
   `eventID` int(13) NOT NULL,
-  `eventNavn` int(128) NOT NULL,
+  `eventNavn` varchar(128) NOT NULL,
   `dato` date NOT NULL,
   `beskrivelse` varchar(200) NOT NULL,
   `bilde` blob DEFAULT NULL,
   `besøkende` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `event`
+--
+
+INSERT INTO `event` (`eventID`, `eventNavn`, `dato`, `beskrivelse`, `bilde`, `besøkende`) VALUES
+(1, 'Julebord', '2023-12-20', 'Bli med på julebord sammen med jobben', NULL, NULL),
+(2, 'Nyttårsfest', '2023-11-30', 'Feir nyåret sammen med venner ', NULL, NULL),
+(3, 'Trening', '2023-11-14', 'Bli med på fellestrening og kom i form før jul', NULL, NULL),
+(4, 'Pepperkakebaking', '2023-12-18', 'Ta med eget utstyr og kom i julestemming med julebaking', NULL, NULL),
+(5, 'Fellesmiddag', '2022-12-25', 'Middag med familien først juledsg', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `myevent`
+--
+
+CREATE TABLE `myevent` (
+  `myEventID` int(11) NOT NULL,
+  `eventID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `myevent`
+--
+
+INSERT INTO `myevent` (`myEventID`, `eventID`, `userID`) VALUES
+(1, 2, 18),
+(8, 4, 18),
+(10, 2, 21),
+(13, 1, 21),
+(14, 1, 18);
 
 -- --------------------------------------------------------
 
@@ -51,8 +85,16 @@ CREATE TABLE `user` (
   `City` varchar(35) NOT NULL,
   `Zip` int(4) NOT NULL,
   `Passord` varchar(128) NOT NULL,
-  `Kjønn` varchar(6) NOT NULL
+  `Kjønn` varchar(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userID`, `Fornavn`, `Etternavn`, `Epost`, `Tlf`, `City`, `Zip`, `Passord`, `Kjønn`) VALUES
+(18, 'Chrissdfsdgsdffdsddsf', 'Martinos', 'Chris@mail.com', '12345678', 'Kristiansandnes', 4623, '1234', 'Mann'),
+(21, 'Hello', 'Games', 'HeGe@mail.com', '87654321', 'Oslo', 4623, '1234', NULL);
 
 --
 -- Indexes for dumped tables
@@ -63,6 +105,12 @@ CREATE TABLE `user` (
 --
 ALTER TABLE `event`
   ADD PRIMARY KEY (`eventID`);
+
+--
+-- Indexes for table `myevent`
+--
+ALTER TABLE `myevent`
+  ADD PRIMARY KEY (`myEventID`);
 
 --
 -- Indexes for table `user`
@@ -79,13 +127,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `eventID` int(13) NOT NULL AUTO_INCREMENT;
+  MODIFY `eventID` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `myevent`
+--
+ALTER TABLE `myevent`
+  MODIFY `myEventID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userID` int(13) NOT NULL AUTO_INCREMENT;
+  MODIFY `userID` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
